246704736e2acb11716f2a2ea9c960661e8e8ca9
Shaoor Munir <shaoormunir@outlook.com>
2017-11-11 01:12:23 +0500

    Initial commit

---
+++ ui_mainwindow.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(585, 376)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralWidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(90, 120, 401, 81))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(11, 11, 11, 11)
+        self.horizontalLayout.setSpacing(6)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_start = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_start.setObjectName("pushButton_start")
+        self.horizontalLayout.addWidget(self.pushButton_start)
+        self.pushButton_stop = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_stop.setObjectName("pushButton_stop")
+        self.horizontalLayout.addWidget(self.pushButton_stop)
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 585, 28))
+        self.menuBar.setObjectName("menuBar")
+        MainWindow.setMenuBar(self.menuBar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton_start.setText(_translate("MainWindow", "Start Capturing"))
+        self.pushButton_stop.setText(_translate("MainWindow", "Stop Capturing"))
+
